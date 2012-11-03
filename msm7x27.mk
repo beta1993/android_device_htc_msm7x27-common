53d8d79405236cc1af09240bd22caa7cec6a4185
OliverG96 <oliverarafo@gmail.com>
2012-11-03 19:25:27 +0000

    Fix RIL connection issues

--- msm7x27.mk
+++ msm7x27.mk
@@ -101,7 +101,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.disable.fd.plmn.prefix=23402,23410,23411 \
     ro.ril.def.agps.mode=2 \
     ro.ril.hsxpa=2 \
-    ro.ril.gprsclass=12
+    ro.ril.gprsclass=12 \
+    ro.telephony.ril.v3=signalstrength,skipbrokendatacall
 
 # Interfaces
 PRODUCT_PROPERTY_OVERRIDES += \
