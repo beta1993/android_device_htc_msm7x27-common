cd9ac4b7e4d89fe7721ea03b27b7b6d58fee097d
OliverG96 <oliverarafo@gmail.com>
2012-11-04 11:24:21 +0000

    Define continuous-af value for fix build

--- libcamera/QcomCamera.cpp
+++ libcamera/QcomCamera.cpp
@@ -372,6 +372,7 @@ void CameraHAL_FixupParams(CameraParameters &settings)
    const char *preview_frame_rates  = "30,27,24,15";
    const char *preferred_frame_rate = "15";
    const char *frame_rate_range     = "(15,30)";
+   const char *continuous-af          = "caf-off";
 
    settings.set(CameraParameters::KEY_VIDEO_FRAME_FORMAT,
                 CameraParameters::PIXEL_FORMAT_YUV420SP);
